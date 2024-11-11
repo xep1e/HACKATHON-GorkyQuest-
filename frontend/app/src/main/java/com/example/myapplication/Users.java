f81f1379bffa8db22b271122b4884736af93d455
Filch <vlad.filchugov2004@mail.ru>
2024-11-11 15:39:50 +0300

    AddFront

---
+++ frontend/app/src/main/java/com/example/myapplication/Users.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+public class Users {
+    private int id;
+    private String name;
+    private String email;
+
+    // Геттеры и сеттеры
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+}
